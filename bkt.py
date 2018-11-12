c0af8efd5a91ee9799029efd05f14e5df38faa76
mengqinggang <mqg@email.com>
2018-11-12 21:35:00 +0800

    predict one steps

--- bkt.py
+++ bkt.py
@@ -1,20 +1,28 @@
-# bkt: bayes knowledge tracing 
+# BKT(Bayesian Knowledge Tracing) Model
 
 import hmm
 
 
+# BKT model for one knowledge
 class bkt:
 
-    def __init__(self, PL0, PL, PF, PS, PG):
-        self.p_l0 = PL0
-        self.p_learn = PL
-        self.p_forget = PF
-        self.p_slip = PS
-        self.p_guess = PG
+    def __init__(self, pi, pl, pf, ps, pg):
+        self.init = pi
+        self.learn = pl
+        self.forget = pf
+        self.slip = ps
+        self.guess = pg
         
-        hmm_pi = [1.0-self.p_l0, self.p_l0]
-        hmm_hh = [[1.0-self.p_learn, self.p_learn], [self.p_forget, 1.0-self.p_forget]]
-        hmm_ho = [[1.0-self.p_guess, self.p_guess], [self.p_slip, 1.0-self.p_slip]]
+        '''
+            pi [0, 1]
+            hh [0,0 0,1
+                1,0 1,1]
+            ho [0,0 0,1
+                1,0 1,1]
+        '''
+        hmm_pi = [1.0-self.init, self.init]
+        hmm_hh = [[1.0-self.learn, self.learn], [self.forget, 1.0-self.forget]]
+        hmm_ho = [[1.0-self.guess, self.guess], [self.slip, 1.0-self.slip]]
         self.hmm_model = hmm.hmm(hmm_pi, hmm_hh, hmm_ho)
 
 
@@ -27,31 +35,52 @@ class bkt:
 
     
     def baum_welch(self, observation_sequence, delta):
-        self.hmm_model.baum_welch(observation_sequence, len(observation_sequence), delta)
-        self.bkt_parameters(self.hmm_model)
+        alpha = self.hmm_model.baum_welch(observation_sequence, len(observation_sequence), delta)
+        self.update_bkt_parameters(self.hmm_model)
+        return alpha
+    
 
+    def predict_next_step(self, os, delta):
+        alpha = self.baum_welch(os, delta)
+        predicts, obs = self.hmm_model.predict_next_steps(alpha, len(os))
+        return predicts, obs
+
+
+    # Todo
+    # def baum_welch(self, observation_sequence, iterations):
 
     def print_parameters(self):
-        print('p_l0', self.p_l0)
-        print('p_learn', self.p_learn)
-        print('p_slip', self.p_slip)
-        print('p_guess', self.p_guess)
+        print('init: ', self.init)
+        print('learn: ', self.learn)
+        print('forget: ', self.forget)
+        print('slip: ', self.slip)
+        print('guess: ', self.guess)
 
 
-    def bkt_parameters(self, hmm):
-        self.p_l0 = hmm.PI[1]
-        self.p_learn = hmm.HH[0][1]
-        self.p_slip = hmm.HO[1][0]
-        self.p_guess = hmm.HO[0][1]
+    # after baum_welch function, update bkt parameters
+    def update_bkt_parameters(self, hmm):
+        self.init = hmm.pi[1]
+        self.learn = hmm.hh[0][1]
+        self.forget = hmm.hh[1][0]
+        self.slip = hmm.ho[1][0]
+        self.guess = hmm.ho[0][1]
         
     
-a_model = bkt(0.5, 0.5, 0.0, 0.1, 0.05)
+a_model = bkt(0.1, 0.5, 0.0, 0.1, 0.05)
 
-os = [0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1]
+os = [0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1]
+
+print("init parameters:")
+a_model.print_parameters()
 
 print(a_model.forward(os))
 print(a_model.viterbi(os))
+
 a_model.baum_welch(os, 0.001)
+
 a_model.print_parameters()
-print(a_model.forward([1]))
+
+predicts, obs = a_model.predict_next_step(os, 0.01)
+print("predicts: ", predicts)
+print("obs: ", obs)
 
--- hmm.py
+++ hmm.py
@@ -7,271 +7,270 @@ import numpy as np
 class hmm:
     
     '''
-    PI: hidden state init prob vector
-    HH: a hidden state-transition prob matrix ( p(hidden state b | hidden state a) )
-    HO: obversation prob distribution matrix ( p(bversation state a | hidden state a) )
-    H: the number of hidden states
-    O: the number of obeservation states
-    T: the length of obeservation sequence
+        pi: hidden state init prob vector
+        hh: a hidden state-transition prob matrix ( p(hidden state b | hidden state a) )
+        ho: observation prob distribution matrix ( p(bversation state a | hidden state a) )
+        h: the number of hidden states
+        o: the number of obeservation states
     '''
-    def __init__(self, PI, HH, HO):
-        self.PI = np.array(PI, np.float)
-        self.HH = np.array(HH, np.float)
-        self.HO = np.array(HO, np.float)
-        self.H = self.HH.shape[0]
-        self.O = self.HO.shape[1]
+    def __init__(self, pi, hh, ho):
+        self.pi = np.array(pi, np.float)
+        self.hh = np.array(hh, np.float)
+        self.ho = np.array(ho, np.float)
+        self.h = self.hh.shape[0]
+        self.o = self.ho.shape[1]
 
 
     # computing the prob of observation sequence given a HMM model parameters
     # os: observation sequence
-    # T: the length of os
-    def forward(self, os, T):
-        alpha = np.zeros((T, self.H))
+    # os_length: the length of os
+    # Todo: check data, exception handle
+    def forward(self, os, os_length):
+        alpha = np.zeros((os_length, self.h))
 
         # 1. Initialization
-        for h in range(self.H):
-            alpha[0, h] = self.PI[h] * self.HO[h, os[0]]
+        for h in range(self.h):
+            alpha[0, h] = self.pi[h] * self.ho[h, os[0]]
 
         # 2. Induction
         # t: time index
-        # i, j: hidden state index
-        for t in range(1, T):
-            for i in range(self.H):
+        # i: time t+1 hidden state index
+        # j: time t hidden state index
+        for t in range(1, os_length):
+            for i in range(self.h):
                 sum_prob = 0.0
-                for j in range(self.H):
-                    sum_prob += alpha[t-1][j] * self.HH[j][i]
-                alpha[t][i] = sum_prob * self.HO[i][os[t]]
+                for j in range(self.h):
+                    sum_prob += alpha[t-1][j] * self.hh[j][i]
+                alpha[t][i] = sum_prob * self.ho[i][os[t]]
 
         # 3. Termination
-        pprob = 0.0
-        for i in range(self.H):
-            pprob += alpha[T-1][i]
+        prob = 0.0
+        for i in range(self.h):
+            prob += alpha[os_length-1][i]
 
-        return pprob 
+        return prob 
+
+
+    def backward(self, os, os_length):
+        beta = np.zeros((os_length, self.h), np.float)
+
+        for h in range(self.h):
+            beta[os_length-1][h] = 1.0
+
+        for t in range(os_length-2, -1, -1):
+            for i in range(self.h):
+                sum_prob = 0.0
+                for j in range(self.h):
+                    sum_prob+= self.hh[i][j] * self.ho[j][os[t+1]] * beta[t+1][j] 
+                beta[t][i] = sum_prob
+
+        prob = 0.0
+        for h in range(self.h):
+            prob += beta[0][h]
+
+        return beta, prob
 
 
     # seeking the most probable hidden state sequence
     # hss: hidden state sequence
-    def viterbi(self, os, T):
-        hss = np.zeros(T, np.int)
+    def viterbi(self, os, os_length):
+        hss = np.zeros(os_length, np.int)
 
-        delta = np.zeros((T, self.H), np.float)
-        psi = np.zeros((T, self.H), np.float)
+        delta = np.zeros((os_length, self.h), np.float)
+        psi = np.zeros((os_length, self.h), np.float)
 
         # 1. Initialization
-        for h in range(self.H):
-            delta[0][h] = self.PI[h] * self.HO[h, os[0]]
+        for h in range(self.h):
+            delta[0][h] = self.pi[h] * self.ho[h, os[0]]
             psi[0][h] = 0
 
         # 2. Recursion
-        for t in range(1, T):
-            for i in range(self.H):
+        for t in range(1, os_length):
+            for i in range(self.h):
                 max_val = 0.0
                 max_val_ind = 0
-                for j in range(self.H):
-                    val = delta[t-1][j] * self.HH[j][i]
+                for j in range(self.h):
+                    val = delta[t-1][j] * self.hh[j][i]
                     if val > max_val:
                         max_val = val
                         max_val_ind = j
-                delta[t][i] = max_val * self.HO[i][os[t]]
+                delta[t][i] = max_val * self.ho[i][os[t]]
                 psi[t][i] = max_val_ind
 
-        # 3. Termination
-        big_prob = delta[T-1, :].max()
+        # 3. os_lengthermination
+        big_prob = delta[os_length-1, :].max()
 
         # 4. Path(hidden state sequence) backtracking
-        hss[T-1] = delta[T-1, :].argmax()
-        for t in range(T-2, -1, -1):
+        hss[os_length-1] = delta[os_length-1, :].argmax()
+        for t in range(os_length-2, -1, -1):
             hss[t] = psi[t+1, hss[t+1]]
 
         return hss
 
 
-    def backward(self, os, T):
-        beta = np.zeros((T, self.H), np.float)
-
-        for h in range(self.H):
-            beta[T-1][h] = 1.0
-
-        for t in range(T-2, -1, -1):
-            for i in range(self.H):
-                sum_prob = 0.0
-                for j in range(self.H):
-                    sum_prob+= self.HH[i][j] * self.HO[j][os[t+1]] * beta[t+1][j] 
-                beta[t][i] = sum_prob
-
-        pprob = 0.0
-        for h in range(self.H):
-            pprob += beta[0][h]
-
-        return beta, pprob
-
-
-    def forward_with_scale(self, os, T):
-        scale = np.zeros(T, np.float)
-        alpha = np.zeros((T, self.H), np.float)
+    def forward_with_scale(self, os, os_length):
+        scale = np.zeros(os_length, np.float)
+        alpha = np.zeros((os_length, self.h), np.float)
         
         # 1. Initialization
-        for h in range(self.H):
-            alpha[0][h] = self.PI[h] * self.HO[h, os[0]]
+        for h in range(self.h):
+            alpha[0][h] = self.pi[h] * self.ho[h, os[0]]
             scale[0] += alpha[0][h]
 
-        for i in range(self.H):
+        for i in range(self.h):
             alpha[0][i] /= scale[0]
 
         # 2. Induction
-        for t in range(1, T):
-            for i in range(self.H):
+        for t in range(1, os_length):
+            for i in range(self.h):
                 sum_prob = 0.0
-                for j in range(self.H):
-                    sum_prob += alpha[t-1][j] * self.HH[j][i]
-                alpha[t][i] = sum_prob * self.HO[i][os[t]]
+                for j in range(self.h):
+                    sum_prob += alpha[t-1][j] * self.hh[j][i]
+                alpha[t][i] = sum_prob * self.ho[i][os[t]]
                 scale[t] += alpha[t][i]
 
-            for h in range(self.H):
+            for h in range(self.h):
                 alpha[t][h] /= scale[t]
 
-        # 3. Termination
+        # 3. os_lengthermination
         log_likelihood = 0.0
-        for t in range(T):
+        for t in range(os_length):
             log_likelihood += math.log(scale[t])
 
         return alpha, log_likelihood, scale
 
     
     # scale: from the forward_with_scale function
-    def backward_with_scale(self, os, T, scale):
-        beta = np.zeros((T, self.H), np.float)
+    def backward_with_scale(self, os, os_length, scale):
+        beta = np.zeros((os_length, self.h), np.float)
 
-        for h in range(self.H):
-            beta[T-1][h] = 1.0/scale[T-1]
+        for h in range(self.h):
+            beta[os_length-1][h] = 1.0/scale[os_length-1]
 
         # i: time t
         # j: time t+1
-        for t in range(T-2, -1, -1):
-            for i in range(self.H):
+        for t in range(os_length-2, -1, -1):
+            for i in range(self.h):
                 sum_prob = 0.0
-                for j in range(self.H):
-                    sum_prob += self.HH[i][j] * self.HO[j][os[t+1]] * beta[t+1][j] 
+                for j in range(self.h):
+                    sum_prob += self.hh[i][j] * self.ho[j][os[t+1]] * beta[t+1][j] 
                 beta[t][i] = sum_prob / scale[t]
 
         return beta 
 
 
-    def computer_gamma(self, T, alpha, beta):
-        gamma = np.zeros((T, self.H), np.float)
+    def computer_gamma(self, os_length, alpha, beta):
+        gamma = np.zeros((os_length, self.h), np.float)
 
-        for t in range(T):
+        for t in range(os_length):
             denominator = 0.0
-            for i in range(self.H):
+            for i in range(self.h):
                 gamma[t][i] = alpha[t][i] * beta[t][i]
                 denominator += gamma[t][i]
 
-            for j in range(self.H):
+            for j in range(self.h):
                 gamma[t][j] = gamma[t][j] / denominator
 
         return gamma
 
 
-    def computer_xi(self, os, T, alpha, beta):
-        xi = np.zeros((T-1, self.H, self.H), np.float)
+    def computer_xi(self, os, os_length, alpha, beta):
+        xi = np.zeros((os_length-1, self.h, self.h), np.float)
         
-        for t in range(T-1):
+        for t in range(os_length-1):
             denominator = 0.0
-            for i in range(self.H):
-                for j in range(self.H):
-                    xi[t][i][j] = alpha[t][i] * self.HH[i][j] * self.HO[j][os[t+1]] * beta[t+1][j]
+            for i in range(self.h):
+                for j in range(self.h):
+                    xi[t][i][j] = alpha[t][i] * self.hh[i][j] * self.ho[j][os[t+1]] * beta[t+1][j]
                     denominator += xi[t][i][j]
             
-            for i in range(self.H):
-                for j in range(self.H):
+            for i in range(self.h):
+                for j in range(self.h):
                     xi[t][i][j] /= denominator
 
         return xi
 
 
     # optimizing model parameters
-    def baum_welch(self, os, T, delta):
+    def baum_welch(self, os, os_length, delta):
         loop_number = 0
         
-        alpha, log_prob, scale = self.forward_with_scale(os, T)
-        beta = self.backward_with_scale(os, T, scale)
-        gamma = self.computer_gamma(T, alpha, beta)
-        xi = self.computer_xi(os, T, alpha, beta)
+        alpha, log_prob, scale = self.forward_with_scale(os, os_length)
+        beta = self.backward_with_scale(os, os_length, scale)
+        gamma = self.computer_gamma(os_length, alpha, beta)
+        xi = self.computer_xi(os, os_length, alpha, beta)
         log_prob_prev = log_prob
         
         while True:
             # reestimate frequency of hidden state i in time t=0
-            for i in range(self.H):
-                self.PI[i] = 0.001 + 0.999 * gamma[0][i]
+            for i in range(self.h):
+                self.pi[i] = 0.001 + 0.999 * gamma[0][i]
 
             # reestimate transition matrix and symbol prob in each state
-            for i in range(self.H):
+            for i in range(self.h):
                 denominator_A = 0.0
-                for t in range(T-1):
+                for t in range(os_length-1):
                     denominator_A += gamma[t][i]
 
-                for j in range(self.H):
+                for j in range(self.h):
                     numerator_A = 0.0
-                    for t in range(T-1):
+                    for t in range(os_length-1):
                         numerator_A += xi[t][i][j]
 
-                    self.HH[i][j] = 0.001 + 0.999 * numerator_A / denominator_A
+                    self.hh[i][j] = 0.001 + 0.999 * numerator_A / denominator_A
 
-                denominator_B = denominator_A + gamma[T-1][i]
-                for k in range(self.O):
+                denominator_B = denominator_A + gamma[os_length-1][i]
+                for k in range(self.o):
                     numerator_B = 0.0
-                    for t in range(T):
+                    for t in range(os_length):
                         if os[t] == k:
                             numerator_B += gamma[t][i]
                         
-                    self.HO[i][k] = 0.001 + 0.999 * numerator_B / denominator_B
+                    self.ho[i][k] = 0.001 + 0.999 * numerator_B / denominator_B
 
-            alpha, log_prob, scale = self.forward_with_scale(os, T)
-            beta = self.backward_with_scale(os, T, scale)
-            gamma = self.computer_gamma(T, alpha, beta)
-            xi = self.computer_xi(os, T, alpha, beta)
+            alpha, log_prob, scale = self.forward_with_scale(os, os_length)
+            beta = self.backward_with_scale(os, os_length, scale)
+            gamma = self.computer_gamma(os_length, alpha, beta)
+            xi = self.computer_xi(os, os_length, alpha, beta)
 
-            d = log_prob - log_prob_prev
+            now_delta = log_prob - log_prob_prev
             log_prob_prev = log_prob
             loop_number += 1
 
-            if d <= delta:
+            if now_delta <= delta:
                 break
 
-        return self.PI, self.HH, self.HO, loop_number
-    
+        #return self.pi, self.hh, self.ho, loop_number, alpha
+        return alpha
 
-#A = [[0.500, 0.375, 0.125],
-#        [0.250, 0.125, 0.625],
-#        [0.250, 0.375, 0.375]]
-#
-#B = [[0.60, 0.20, 0.15, 0.05],
-#        [0.25, 0.25, 0.25, 0.25],
-#        [0.05, 0.10, 0.35, 0.50]]
-#
-#PI = [0.63, 0.17, 0.20]
-#
-#ob = [0, 2, 3]
-#T = 3
-#
-#model = hmm(PI, A, B)
-#print(model.forward(ob, T))
-#
-#A =  [[0.500, 0.375, 0.125],
-#          [0.250, 0.125, 0.625],
-#          [0.250, 0.375, 0.375]]
-#          
-#B = [[0.50, 0.50],
-# [0.75, 0.25],
-# [0.25, 0.75]]
-#
-#PI = [0.333, 0.333, 0.333]
-#
-#ob = [0, 0, 0, 0, 1, 0, 1, 1, 1, 1]
-#T = len(ob)
-#
-#model = hmm(PI, A, B)
-#print(model.viterbi(ob, T))
-#print(model.baum_welch(ob, T, 0.00001))
+
+    def predict_next_steps(self, alpha, os_length):
+        # next time states probability
+        states_predictions = np.zeros((os_length, self.h), np.float)
+        # next time observations probability
+        observations_predictions = np.zeros((os_length, self.o), np.float)
+        
+        # 1. Initialization states_predictions
+        for h in range(self.h):
+            states_predictions[0][h] = self.pi[h]
+
+        # 2. Induction states_predictions
+        for t in range(1, os_length):
+            for i in range(self.h):
+                sum_prob = 0.0
+                for j in range(self.h):
+                    sum_prob += alpha[t-1][j] * self.hh[j][i]
+                
+                states_predictions[t][i] = sum_prob
+
+        # 3. compute observations_predictions
+        for t in range(os_length):
+            for i in range(self.o):
+                sum_prob = 0.0
+                for j in range(self.h):
+                    sum_prob += states_predictions[t][j] * self.ho[j][i]
+                    
+                observations_predictions[t][i] = sum_prob
+
+        return states_predictions, observations_predictions
 
